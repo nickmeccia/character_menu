2aed809df90b64ed713d86b55e68d918ef59d098
nickmeccia <nickmeccia@gmail.com>
2013-02-11 11:31:30 -0600

    Improved typography.

--- src/character_menu/view/index.hiccup.clj
+++ src/character_menu/view/index.hiccup.clj
@@ -2,10 +2,11 @@
  [:div.sidebar]
 
  [:div.main
-  [:div.title
    [:hgroup
     [:h1 "mark g"]
-    [:h2 "level 1 barbarian"]]]
+    [:h2 "level 1 mage"]
+    [:a {:href "mailto:nickmeccia@gmail.com"} "Report a Completed Quest"]
+]
 
   [:section
    [:h3 "quests"]
@@ -22,42 +23,49 @@
      [:h4 "Fibonacci Scale"]]
     [:li.locked
      [:h4 "Revise Tic Tac Toe"]]
-  [:li.locked
-   [:h4 "Design a Blog"]]
     [:li.locked
-     [:h4 "Design a Portfolio"]]]]
+     [:h4 "Design a Blog"]]
+    [:li.locked
+     [:h4 "Design a Portfolio"]]
+  ]]
 
   [:section
    [:h3 "side quests"]
    [:ol
-    [:li
+    [:li.active
      [:h4 "Read Prince of Cats"]]
-    [:li
-     [:h4 [:a {:href "http://asdf.com/"} "Watch John Cleese"]]]]]
-
-   [:section
-    [:h3 "tomes"]
-    [:ol
-     [:li.completed
-      [:h4 "Thinking With Type"]
-      [:h5 "by Ellen Lupton"]]
-     [:li.completed
-      [:h4 "Ordering Disorder: Grid Principles for Web Design"]
-      [:h5 "by Khoi Vinh"]]
-     [:li.locked
-      [:h4 "Making and Breaking the Grid"]
-      [:h5 "Some Dude"]]]]
-
-    [:section.last
-     [:h3 "records"]
-     [:ol
-      [:li.active "blog post for week 1"]
-      [:li.active "blog post for week 2"]
-      [:li.active "blog post for week 3"]
-      [:li.active "blog post for week 4"]
-      [:li.active "blog post for week 5"]
-      [:li.active "blog post for week 6"]
-      [:li.active "blog post for week 7"]
-      [:li.active "blog post for week 8"]
-      ]]
+    [:li.active
+     [:h4 [:a {:href "http://www.youtube.com/watch?v=VShmtsLhkQg"} "Watch John Cleese"]]]
+    [:li.active
+     [:h4 [:a {:href "http://vimeo.com/17079380"} "Watch More Perfect Typography"]]]
   ]]
+
+  [:section
+   [:h3 "tomes"]
+   [:ol
+    [:li.completed
+     [:h4 "Thinking With Type"]
+     [:h5 "by Ellen Lupton"]]
+    [:li.completed
+     [:h4 "Ordering Disorder: Grid Principles for Web Design"]
+     [:h5 "by Khoi Vinh"]]
+    [:li.locked
+     [:h4 "Making and Breaking the Grid"]
+     [:h5 "Some Dude"]]
+  ]]
+
+  [:section.last
+   [:h3 "records"]
+   [:ol
+    [:li.active
+     [:h4 "blog post for week 1"]]
+    [:li.active
+     [:h4 "blog post for week 2"]]
+    [:li.active
+     [:h4 "blog post for week 3"]]
+    [:li.locked
+     [:h4 "blog post for week 4"]]
+    [:li.locked
+     [:h4 "blog post for week 5"]]
+  ]]
+]]
