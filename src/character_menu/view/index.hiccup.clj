e9b758ae7b63833390ac912f0a82b21f6f5c3b7a
nickmeccia <nickmeccia@gmail.com>
2013-02-12 09:22:08 -0600

    Properly aligned the type for list items that have more than one line.

--- src/character_menu/view/index.hiccup.clj
+++ src/character_menu/view/index.hiccup.clj
@@ -49,6 +49,9 @@
     [:li.completed
      [:h4 "Ordering Disorder: Grid Principles for Web Design"]
      [:h5 "by Khoi Vinh"]]
+    [:li.active
+     [:h4 "1000 Type Treatments"]
+     [:h5 "by Wilson Harvey"]]
     [:li.locked
      [:h4 "Making and Breaking the Grid"]
      [:h5 "Some Dude"]]
