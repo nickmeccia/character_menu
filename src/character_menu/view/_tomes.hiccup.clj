e077a0861f98474985a4d4f45c6d228ecd69d616
nickmeccia <nickmeccia@gmail.com>
2013-02-20 15:15:43 -0600

    Added assignments as text files.

--- src/character_menu/view/_tomes.hiccup.clj
+++ src/character_menu/view/_tomes.hiccup.clj
@@ -7,9 +7,12 @@
   [:li.completed
    [:h4 "Ordering Disorder: Grid Principles for Web Design"]
    [:h5 "by Khoi Vinh"]]
-  [:li.active
+  [:li.completed
    [:h4 "1000 Type Treatments"]
    [:h5 "by Wilson Harvey"]]
+  [:li.active
+   [:h4 "Design Elements: Color Fundamentals"]
+   [:h5 "by Aaris Sherin"]]
   [:li.locked
    [:h4 "Making and Breaking the Grid"]
    [:h5 "Some Dude"]]
